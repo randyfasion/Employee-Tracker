f0a87eccb1ecd3ad84b9f3fb4b70a7738d228472
Randy Faison <randyfaison@randys-mbp.myfiosgateway.com>
2021-05-13 17:32:59 -0400

    prompt list of options

--- server.js
+++ server.js
@@ -15,3 +15,52 @@ const connection = mysql.createConnection({
   database: 'employeetracker_db',
 });
 
+connection.connect((err) => {
+    if (err) throw err;
+    runSearch();
+  });
+  
+  const runSearch = () => {
+    inquirer
+      .prompt({
+        name: 'action',
+        type: 'rawlist',
+        message: 'What would you like to do?',
+        choices: [
+          'View all Employees',
+          'View all Departments',
+          'View all Roles',
+          'Add Employee',
+          'Add Role',
+          'Add Department',
+          'Update Employee roles'
+        ],
+      })
+      .then((answer) => {
+        switch (answer.action) {
+          case 'Find songs by artist':
+            artistSearch();
+            break;
+  
+          case 'Find all artists who appear more than once':
+            multiSearch();
+            break;
+  
+          case 'Find data within a specific range':
+            rangeSearch();
+            break;
+  
+          case 'Search for a specific song':
+            songSearch();
+            break;
+  
+          case 'Find artists with a top song and top album in the same year':
+            songAndAlbumSearch();
+            break;
+  
+          default:
+            console.log(`Invalid action: ${answer.action}`);
+            break;
+        }
+      });
+  };

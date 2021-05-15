dda56423d8e0de5b0cc6878ab972d8b2517fd9d9
Randy Faison <randyfaison@randys-mbp.myfiosgateway.com>
2021-05-15 19:39:47 -0400

    added comments to server.js

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const inquirer = require('inquirer');
 const connection = mysql.createConnection({
   host: 'localhost',
 
-  // Your port; if not 3306
+  // Port
   port: 3306,
 
   // Your username
   user: 'root',
 
-  // Be sure to update with your own MySQL password!
+  // password and database
   password: '',
   database: 'employeetracker_db',
 });
